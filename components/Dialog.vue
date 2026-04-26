949d03cc4b0d836f0bfc1658b1c41f7a7e9dafd6
Vyacheslav Odue <54174788+vvl1m@users.noreply.github.com>
2026-04-27 01:19:56 +0500

    hotfix: ПЛАТНО

--- components/Dialog.vue
+++ components/Dialog.vue
@@ -42,7 +42,7 @@ async function sendMessage() {
                 const { data:checkAvaliability, error: erroCheck } = await supabase
                     .from('files_metadata')
                     .select('*')
-                if (checkAvaliability.length > 40) {
+                if (checkAvaliability.length > 1) {
                     throw new Error('Лимит по файлам превышен! (теперь файлы платные)');
                 }
                 const { data, error } = supabase
