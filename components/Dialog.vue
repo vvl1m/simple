b85e3d7502260710c6a89564e9bed31e1f23035f
Vyacheslav Odue <54174788+vvl1m@users.noreply.github.com>
2026-04-26 23:06:11 +0500

    hotfix: лимит файлов теперь 40

--- components/Dialog.vue
+++ components/Dialog.vue
@@ -42,7 +42,7 @@ async function sendMessage() {
                 const { data:checkAvaliability, error: erroCheck } = await supabase
                     .from('files_metadata')
                     .select('*')
-                if (checkAvaliability.length > 5) {
+                if (checkAvaliability.length > 50) {
                     throw new Error('Лимит по файлам превышен! (теперь файлы платные)');
                 }
                 const { data, error } = supabase
